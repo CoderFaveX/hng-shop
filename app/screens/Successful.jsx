ca15fdcec669917c62cee8702f87be78161a2aa9
CoderFaveX <faveejiofor2009@gmail.com>
2024-07-14 02:28:14 +0100

    Commiting bg

--- app/screens/Successful.jsx
+++ app/screens/Successful.jsx
@@ -1,20 +1,24 @@
 import React from "react";
 import MontText from "../components/MontText";
-import { Image, SafeAreaView, StyleSheet, View } from "react-native";
+import { Image, SafeAreaView, StyleSheet, ImageBackground, View } from "react-native";
 import Header from "../context/Header";
 
 const Successful = () => {
+    const { height: viewportHeight } = Dimensions.get('window');
+
     return (
         <SafeAreaView style={styles.container}>
-            <Header header="" />
-            <MontText style={{ fontSize: 20, color: "#2A2A2A", marginHorizontal: "auto", marginVertical: 20 }}>Payment Successful</MontText>
-            <View style={{ marginTop: 40, marginHorizontal: "auto" }}>
-                <View style={{ backgroundColor: "#FF7F7D", padding: 20, width: 100, borderRadius: 120, marginHorizontal: "auto" }}>
-                    <Image style={{ width: 67, height: 67 }} source={require("../../assets/images/checks.png")} />
+            <ImageBackground source={require('../../assets/images/confetti.png')} style={[styles.heroBackground, { height: viewportHeight - 180 }]}>
+                <Header header="" />
+                <MontText style={{ fontSize: 20, color: "#2A2A2A", marginHorizontal: "auto", marginVertical: 20 }}>Payment Successful</MontText>
+                <View style={{ marginTop: 40, marginHorizontal: "auto" }}>
+                    <View style={{ backgroundColor: "#FF7F7D", padding: 20, width: 100, borderRadius: 120, marginHorizontal: "auto" }}>
+                        <Image style={{ width: 67, height: 67 }} source={require("../../assets/images/checks.png")} />
+                    </View>
+                    <MontText style={{ fontSize: 18, textAlign: "center", marginVertical: 10 }}>Payment Successful</MontText>
+                    <MontText style={{ textAlign: "center" }} monttype={"Mont"}>Thanks for your purchase</MontText>
                 </View>
-                <MontText style={{ fontSize: 18, textAlign: "center", marginVertical: 10 }}>Payment Successful</MontText>
-                <MontText style={{ textAlign: "center" }} monttype={"Mont"}>Thanks for your purchase</MontText>
-            </View>
+            </ImageBackground>
         </SafeAreaView>
     );
 }
@@ -24,6 +28,13 @@ const styles = StyleSheet.create({
         flex: 1,
         backgroundColor: "#FAFAFA",
         paddingHorizontal: 15,
+    },
+    heroBackground: {
+        width: '100%',
+        justifyContent: 'center',
+        alignItems: 'center',
+        borderRadius: 10,
+        overflow: 'hidden',
     },
 });
 
